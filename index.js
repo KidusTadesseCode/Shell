cea4dc82a8b7ffd1ca3f9f7f15c4dd53576ac378
Kidus Arega <Kidustadessearega@gmail.com>
2025-07-27 01:49:01 -0400

    commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { runCommands } from "./src/command-runner.js";
 import chalk from "chalk";
 import path from "path";
 import { markMarkdownFile, checkIfFileIsMarked } from "./src/file-mark.js";
+import { prismaCoder } from "./src/coder/prismaCoder.js";
 // const clear = "\x1Bc";
 // console.log(clear);
 
@@ -21,9 +22,10 @@ const main = async () => {
     const isMarked = await checkIfFileIsMarked(markdownPath);
     console.log("isMarked: ", isMarked);
     if (!isMarked) return;
-    const { filesToDistribute, commandsToRun } = parseMarkdown(markdownPath);
+    let { filesToDistribute, commandsToRun } = parseMarkdown(markdownPath);
     if (filesToDistribute.length > 0) {
       console.log(chalk.cyan("\n--- Distributing Files ---"));
+      filesToDistribute = await prismaCoder(filesToDistribute);
       writeFiles(filesToDistribute);
     } else {
       console.log(chalk.yellow("No files to distribute."));
